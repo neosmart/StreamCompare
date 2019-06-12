c1831807bddec256622ed2139a49d65d25b463e2
Mahmoud Al-Qudsi <mqudsi@neosmart.net>
2019-06-11 20:04:40 -0500

    Add fast-path for Memory.Compare() using Span.SequenceEqual where available
    This includes retargeting the project to build for both .NET Standard
    2.0 (the latest version currently available) and .NET Core 2.2 (the only
    released framework version with the needed implementation).

--- StreamCompare/Memory.cs
+++ StreamCompare/Memory.cs
@@ -7,12 +7,21 @@ namespace StreamCompare
 {
     internal class Memory
     {
+#if NETCOREAPP2_2 || NETCOREAPP3_0
+        // Using the platform-native Span<T>.SequenceEqual<T>(..)
+        public static bool Compare(byte[] range1, int offset1, byte[] range2, int offset2, int count)
+        {
+            var span1 = range1.AsSpan(offset1, count);
+            var span2 = range2.AsSpan(offset2, count);
+
+            return span1.SequenceEqual(span2);
+        }
+#else
         // The most basic implementation, in platform-agnostic, safe C#
         public static bool Compare(byte[] range1, int offset1, byte[] range2, int offset2, int count)
         {
-            Debug.Assert(range1.Length == range2.Length);
-
-            for (int i = 0; i < count; ++i)
+            // Working backwards lets the compiler optimize away bound checking after the first loop
+            for (int i = count - 1; i >= 0; ++i)
             {
                 if (range1[offset1 + i] != range2[offset2 + i])
                 {
@@ -22,5 +31,6 @@ namespace StreamCompare
 
             return true;
         }
+#endif
     }
 }
